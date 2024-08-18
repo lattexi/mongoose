bffc40e15fb9059563f01bc3f5b057a756172373
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-08-18 20:46:57 +0300

    first commit

---
+++ test/api.test.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-loss-of-precision */
+import mongoose from 'mongoose';
+import app from '../src/app';
+import {Types} from 'mongoose';
+// const app = 'http://localhost:3000';
+
+describe('GET /api/v1', () => {
+  beforeAll(async () => {
+    if (!process.env.DB_URL) {
+      throw new Error('DB_URL is not defined');
+    }
+    await mongoose.connect(process.env.DB_URL);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
