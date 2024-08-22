83df79d5e3130e5ce0cbc115e710ae90de2941f7
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-08-23 00:48:02 +0300

    eslint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,9 @@
 import express, {Request, Response} from 'express';
 import {MessageResponse} from '../types/Messages';
-import {AnyObject} from 'mongoose';
 
 const router = express.Router();
 
-router.get<AnyObject, MessageResponse>('/', (_req: Request, res: Response) => {
+router.get<{}, MessageResponse>('/', (_req: Request, res: Response) => {
   res.json({
     message: 'api v1',
   });
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ import cors from 'cors';
 import {notFound, errorHandler} from './middlewares';
 import api from './api';
 import {MessageResponse} from './types/Messages';
-import {AnyObject} from 'mongoose';
 
 const app = express();
 
@@ -17,7 +16,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<AnyObject, MessageResponse>('/', (_req: Request, res: Response) => {
+app.get<{}, MessageResponse>('/', (_req: Request, res: Response) => {
   res.json({
     message: 'API location: api/v1',
   });
